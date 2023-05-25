772cb5ece80f95421c420682b0f6533c590ef55f
arsitaocti <sitarsita0810@gmail.com>
2023-05-25 01:32:56 -0500

    fixed base

--- app/Views/base.php
+++ app/Views/base.php
@@ -1,18 +1,5 @@
 <html>
     <head>
-        <!-- <?= $this->include('layout/head') ?> -->
-        <?= $this->include('layouts/head') ?>
-    </head>
-    <body>
-        <?= $this->include('layouts/navbar') ?>
-
-        <main role="main " class="container">
-
-            <?= $this->renderSection('content') ?>
-        </main>
-        <?= $this->include('layouts/footer') ?>
-
-        <?= $this->include('layouts/scripts') ?>
         <?= $this->include('layout/head') ?>
     </head>
     <body>
@@ -26,6 +13,5 @@
         <?= $this->include('layout/footer') ?>
 
         <?= $this->include('layout/script') ?>
->>>>>>> 499d2465ef258e68fa13ad5937916d9f7db06d21
     </body>
 </html>
--- app/Views/layout/head.php
+++ app/Views/layout/head.php
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-<head>
-=======
 <!DOCTYPE html>
 <html lang="en">
    <head>
->>>>>>> 499d2465ef258e68fa13ad5937916d9f7db06d21
       <!-- basic -->
       <meta charset="utf-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -18,24 +14,6 @@
       <meta name="description" content="">
       <meta name="author" content="">
       <!-- bootstrap css -->
-<<<<<<< HEAD
-      <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-      <!-- style css -->
-      <link rel="stylesheet" type="text/css" href="css/style.css">
-      <!-- Responsive-->
-      <link rel="stylesheet" href="css/responsive.css">
-      <!-- fevicon -->
-      <link rel="icon" href="images/fevicon.png" type="image/gif" />
-      <!-- Scrollbar Custom CSS -->
-      <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
-      <!-- Tweaks for older IEs-->
-      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
-      <!-- owl stylesheets --> 
-      <link rel="stylesheet" href="css/owl.carousel.min.css">
-      <link rel="stylesheet" href="css/owl.theme.default.min.css">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
-   </head>
-=======
       <link rel="stylesheet" type="text/css" href="/aset/css/bootstrap.min.css">
       <!-- style css -->
       <link rel="stylesheet" type="text/css" href="/aset/css/style.css">
@@ -53,4 +31,3 @@
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
    </head>
  
->>>>>>> 499d2465ef258e68fa13ad5937916d9f7db06d21
