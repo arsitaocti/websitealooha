9e0aa5483ba2bb9265c7a52d465714a0b524c798
arsitaocti <sitarsita0810@gmail.com>
2023-05-25 01:15:01 -0500

    fixed pages bugs

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -1,9 +1,7 @@
-
 <?php
 
 namespace App\Controllers;
-
-use CodeIgniter\Debug\Toolbar\Collectors\Views;
+use App\Controllers\BaseController;
 
 class Pages extends BaseController
 {
@@ -20,26 +18,4 @@ class Pages extends BaseController
         echo view('pages/about');
         echo view('layout/footer');
     }
-<?php
-
-namespace App\Controllers;
-
-use CodeIgniter\Debug\Toolbar\Collectors\Views;
-
-class Pages extends BaseController
-{
-    public function index()
-    {
-      return view ('product/index');
-      
-    }
-
-    public function about ()
-
-    {
-        echo view ('pages/about');
-        echo view('pages/about');
-        echo view('layout/footer');
-    }
->>>>>>> 499d2465ef258e68fa13ad5937916d9f7db06d21
 }
